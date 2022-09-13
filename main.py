da06ca068ba76bd23e676adb83de08adcde8c51c
Andreas Gut <andreasgut@gmx.ch>
2022-09-13 20:36:14 +0200

    Mehrstimmigkeit hinzugefügt (Problem: Midinoten werden nicht mehr empfangen)

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import copy
 import threading
+import time
 import tkinter
 from tkinter import Tk, StringVar, HORIZONTAL, Scale
 from tkinter.ttk import *
 
-import midi
-
 import rtmidi
-from rtmidi import MidiMessage
 
 threads = []
 midiin = rtmidi.RtMidiIn()
 midiout = rtmidi.RtMidiOut()
 rec = False
+added_interval = 0
 
 
 def print_message(midi):
@@ -44,6 +43,11 @@ def get_midi_devices_out():
     return devices
 
 
+def get_intervals():
+    return ["Ohne", "Kleine Sekunde", "Grosse Sekunde", "Kleine Terz", "Grosse Terz", "Quarte",
+            "Tritonus", "Quinte", "Kleine Sexte", "Grosse Sexte", "Kleine Septime", "Grosse Septime"]
+
+
 def connect_midi_in(selection):
     midiin.closePort()
     counter = -1
@@ -63,8 +67,19 @@ def connect_midi_out(selection):
             midiout.openPort(counter)
         counter += 1
 
+def set_added_interval(interval):
+    print("Hinzugefügtes Intervall: " + interval)
+    counter = 0
+    global added_interval
+    for i in get_intervals():
+        if i == interval:
+            added_interval = counter
+            print("Verschiebung um " + str(counter) + " Halbtöne")
+        counter += 1
 
-def record():
+
+
+def record(*args):
     global rec
     rec = True
     thread = threading.Thread(target=record_in_thread)
@@ -73,7 +88,7 @@ def record():
 
 def record_in_thread():
     while rec:
-        m = midiin.getMessage(5)  # some timeout in ms
+        m = midiin.getMessage(10)  # some timeout in ms
         if m:
             print_message(m)
             prepare_message(m)
@@ -87,29 +102,30 @@ def prepare_message(message):
     midiout.sendMessage(message)
     original_note = message.getNoteNumber()
 
+    if added_interval != 0:
+        message.setNoteNumber(original_note + added_interval)
+        show_note_in_scale(message, "brown")
+
+
+    color_added_notes = "orange"
+
     if checkbox_minus_2.instate(['selected']):
         message.setNoteNumber(original_note - 24)
         midiout.sendMessage(message)
-        show_note_in_scale(message, "green")
+        show_note_in_scale(message, color_added_notes)
     if checkbox_minus_1.instate(['selected']):
         message.setNoteNumber(original_note - 12)
         midiout.sendMessage(message)
-        show_note_in_scale(message, "green")
+        show_note_in_scale(message, color_added_notes)
     if checkbox_plus_1.instate(['selected']):
         message.setNoteNumber(original_note + 12)
         midiout.sendMessage(message)
-        show_note_in_scale(message, "green")
+        show_note_in_scale(message, color_added_notes)
     if checkbox_plus_2.instate(['selected']):
-        message.setNoteNumber(original_note + 24 )
+        message.setNoteNumber(original_note + 24)
         midiout.sendMessage(message)
-        show_note_in_scale(message, "green")
+        show_note_in_scale(message, color_added_notes)
 
-    # message.setNoteNumber(message.getNoteNumber() + (transposition*12))
-    # midiout.sendMessage(message)
-    #
-    #     if message.isNoteOn():
-    #         note_minus_2 = [0x90, message.getNoteNumber() - 12, 127]  # channel 1, middle C, velocity 112
-    #         send_message(note_minus_2)
 
 
 def show_note_in_scale(message, color):
@@ -128,17 +144,22 @@ def stop():
     rec = False
 
 
+def choose_preset(number):
+    label_chosen_preset.config(text="Preset: " + str(number))
+
+
 # Window
 window = Tk()  # erstellt das Fenster
 window.title("Miditransmitter")  # setzt den Fenstertitel
 window.resizable(width=False, height=False)  # fixiert die Fenstergrösse
 window.configure(background="#ECECEC")
 window.geometry("1200x750")
+window.bind('<r>', record)
 
 label_device = Label(text="Gerät", width=20)
 label_preset = Label(text="Presets", width=20)
-frame_rec_buttons = Frame(window)
-button_record = Button(master=frame_rec_buttons, text="rec.", command=record)
+frame_rec_buttons = Frame(window, width=200)
+button_record = Button(master=frame_rec_buttons, text="rec.", command=record, underline=0)
 button_record.pack()
 button_stop = Button(master=frame_rec_buttons, text="stop", command=stop)
 button_stop.pack()
@@ -207,12 +228,32 @@ for i in range(132):
     notes.append(label)
 
 # Presets
-buttons = []
+buttons_preset = []
 frame_presetbuttons = Frame(window)
-for i in range(10):
-    button = Button(frame_presetbuttons, text=i, width=2)
-    button.pack(side="left")
-    buttons.append(button)
+label_chosen_preset = Label(master=frame_presetbuttons, text="Preset: -")
+label_chosen_preset.pack(side="right")
+
+button1 = Button(frame_presetbuttons, text=1, width=2, command=lambda: choose_preset(1))
+button1.pack(side="left")
+buttons_preset.append(button1)
+
+button2 = Button(frame_presetbuttons, text=2, width=2, command=lambda: choose_preset(2))
+button2.pack(side="left")
+buttons_preset.append(button2)
+
+button3 = Button(frame_presetbuttons, text=3, width=2, command=lambda: choose_preset(3))
+button3.pack(side="left")
+buttons_preset.append(button3)
+
+button4 = Button(frame_presetbuttons, text=4, width=2, command=lambda: choose_preset(4))
+button4.pack(side="left")
+buttons_preset.append(button4)
+
+button5 = Button(frame_presetbuttons, text=5, width=2, command=lambda: choose_preset(5))
+button5.pack(side="left")
+buttons_preset.append(button5)
+
+
 frame_presetbuttons.grid(row=1, column=1)
 
 ports = range(midiin.getPortCount())
@@ -238,6 +279,8 @@ slider_transpose.pack(side="left")
 
 # Added Octaves
 frame_added_octaves = Frame(window)
+label_added_octaves = Label(master=frame_added_octaves, text="Oktaven hinzufügen:")
+label_added_octaves.pack(side="left")
 checkbox_minus_2 = Checkbutton(master=frame_added_octaves, text="-2")
 checkbox_minus_2.pack(side="left")
 checkbox_minus_1 = Checkbutton(master=frame_added_octaves, text="-1")
@@ -247,15 +290,26 @@ checkbox_plus_1.pack(side="left")
 checkbox_plus_2 = Checkbutton(master=frame_added_octaves, text="+2")
 checkbox_plus_2.pack(side="left")
 
+# Added Interval
+frame_added_interval = Frame(window)
+label_added_interval = Label(master=frame_added_interval, text="Intervall hinzufügen:")
+label_added_interval.pack(side="left")
+clicked_intervals = StringVar()
+optionmenu_interval = OptionMenu(frame_added_interval, clicked_intervals, *get_intervals(), command=set_added_interval)
+optionmenu_interval.pack(side="left")
+
+
+
 # Grid
 label_device.grid(row=0, column=0)
 optionmenu_devices_in.grid(row=0, column=1)
 optionmenu_devices_out.grid(row=0, column=2)
 label_preset.grid(row=1, column=0)
 frame_rec_buttons.grid(row=2, column=0)
-frame_delay.grid(row=14, column=0)
+# frame_delay.grid(row=14, column=0)
 frame_transpose.grid(row=15, column=0)
 frame_added_octaves.grid(row=16, column=0)
+frame_added_interval.grid(row=17, column=0)
 
 for i in range(11):
     note_frames[i].grid(row=i + 3, column=0, columnspan=5)
