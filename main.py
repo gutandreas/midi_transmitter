c89e10b136bc89ca6bb390f67e7ad0e05a23b4b8
Andreas Gut <andreasgut@gmx.ch>
2022-09-17 16:20:03 +0200

    Tonale Intervalle für C-Dur und G-Dur hinzugefügt

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ threads = []
 midiin = rtmidi.RtMidiIn()
 midiout = rtmidi.RtMidiOut()
 rec = False
-added_interval = 0
+added_interval_absolute = 0
+added_interval_tonal = 0
 interval_direction = 0
+chosen_tonart = "Keine"
 
 backgroundcolor = "#ECECEC"
 color_1 = "lightblue"
+
+dur_distances = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23]
+moll_distances = [0, 2, 3, 5, 7, 8, 10]
 
 
 def print_message(midi):
@@ -48,13 +53,24 @@ def get_midi_devices_out():
     return devices
 
 
-def get_intervals():
+def get_intervals_absolute():
     return ["Ohne", "Kleine Sekunde", "Grosse Sekunde", "Kleine Terz", "Grosse Terz", "Quarte",
             "Tritonus", "Quinte", "Kleine Sexte", "Grosse Sexte", "Kleine Septime", "Grosse Septime"]
 
 
+def get_intervals_tonal():
+    return ["Ohne", "Sekunde", "Terz", "Quarte", "Quinte", "Sexte", "Septime"]
+
+
 def get_direction():
     return ["Richtung", "Aufwärts", "Abwärts"]
+
+
+def get_tonart():
+    return ["Tonart", "Keine", "C-Dur", "G-Dur", "A-Dur", "E-Dur", "H-Dur", "Fis-Dur", "F-Dur", "B-Dur", "Es-Dur",
+            "As-Dur", "Des-Dur", "Ges-Dur",
+            "a-moll", "e-moll", "fis-moll", "cis-moll", "dis-moll", "d-moll", "g-moll", "c-moll", "f-moll", "b-moll",
+            "es-moll"]
 
 
 def connect_midi_in(selection):
@@ -82,12 +98,20 @@ def connect_midi_out(selection):
 def set_added_interval(interval):
     print("Hinzugefügtes Intervall: " + interval)
     counter = 0
-    global added_interval
-    for i in get_intervals():
-        if i == interval:
-            added_interval = counter
-            print("Verschiebung um " + str(counter) + " Halbtöne")
-        counter += 1
+    global added_interval_absolute
+    global added_interval_tonal
+    if interval in get_intervals_tonal():
+        for i in get_intervals_tonal():
+            if i == interval:
+                added_interval_tonal = counter
+                print("Verschiebung um " + str(counter) + " Tonleiterstufen")
+            counter += 1
+    else:
+        for i in get_intervals_absolute():
+            if i == interval:
+                added_interval_absolute = counter
+                print("Verschiebung um " + str(counter) + " Halbtöne")
+            counter += 1
 
 
 def set_interval_direction(direction):
@@ -98,6 +122,16 @@ def set_interval_direction(direction):
         if d == direction:
             interval_direction = counter
         counter += 1
+
+
+def set_tonart(tonart):
+    print(tonart)
+    global chosen_tonart
+    chosen_tonart = tonart
+    if tonart == "Keine":
+        optionmenu_interval.set_menu(*get_intervals_absolute())
+    else:
+        optionmenu_interval.set_menu(*get_intervals_tonal())
 
 
 def record(*args):
@@ -137,15 +171,32 @@ def prepare_message(message):
     midiout.sendMessage(message)
     original_note = message.getNoteNumber()
 
-    if added_interval != 0:
+    if chosen_tonart == "Keine" and added_interval_absolute != 0:
         if interval_direction == 0:
-            message.setNoteNumber(original_note + added_interval)
+            message.setNoteNumber(original_note + added_interval_absolute)
             midiout.sendMessage(message)
             show_note_in_scale(message, "blue")
         else:
-            message.setNoteNumber(original_note - added_interval)
+            message.setNoteNumber(original_note - added_interval_absolute)
             midiout.sendMessage(message)
             show_note_in_scale(message, "blue")
+    else:
+        number_in_c = message.getNoteNumber() % 12
+        print("number in c: ", number_in_c)
+        delta = 0
+        if chosen_tonart == "C-Dur":
+            delta = 0
+        if chosen_tonart == "G-Dur":
+            delta = 5
+
+        number_in_scale = (number_in_c + delta) % 12
+        print("number in scale: ", number_in_scale)
+        if (number_in_scale in dur_distances):
+            index = dur_distances.index(number_in_scale)
+            difference = dur_distances[index + added_interval_tonal] - dur_distances[index]
+            message.setNoteNumber(original_note + difference)
+            midiout.sendMessage(message)
+            show_note_in_scale(message, "purple")
 
     color_added_notes = "green"
 
@@ -197,21 +248,20 @@ window = ThemedTk(theme="aqua")
 window.title("Miditransmitter")  # setzt den Fenstertitel
 window.resizable(width=False, height=False)  # fixiert die Fenstergrösse
 window.configure(background="#ECECEC")
-window.geometry("900x400")
+window.geometry("900x450")
 
-
-
-frame_rec_buttons = Frame(window)
+frame_rec_buttons = Frame(window, padding=10)
+label_record = Label(master=frame_rec_buttons, text="Verbindung", font=("Helvetica", 15, "bold"))
+label_record.pack()
 button_record = Button(master=frame_rec_buttons, text="rec.", command=record, underline=0, background="lightgreen",
                        highlightbackground=backgroundcolor, state=DISABLED)
-button_record.pack(side="left")
+button_record.pack(side="left", anchor="center")
 button_stop = Button(master=frame_rec_buttons, text="stop", command=stop, underline=0, background="red",
                      highlightbackground=backgroundcolor, state=DISABLED)
-button_stop.pack(side="left")
+button_stop.pack(side="left", anchor="center")
 
-
-frame_input = Frame(window)
-label_input = Label(master=frame_input, text="Eingang")
+frame_input = Frame(window, padding=10)
+label_input = Label(master=frame_input, text="Eingang", font=("Helvetica", 15, "bold"))
 label_input.pack()
 clicked_in = StringVar()
 optionmenu_devices_in = OptionMenu(frame_input, clicked_in, *get_midi_devices_in(), command=connect_midi_in)
@@ -219,8 +269,8 @@ optionmenu_devices_in.pack()
 optionmenu_devices_in.config(width=20)
 # button_update = Button(command=update_midi_devices, text="Update")
 
-frame_output = Frame(window)
-label_output = Label(master=frame_output, text="Ausgang")
+frame_output = Frame(window, padding=10)
+label_output = Label(master=frame_output, text="Ausgang", font=("Helvetica", 15, "bold"))
 label_output.pack()
 clicked_out = StringVar()
 optionmenu_devices_out = OptionMenu(frame_output, clicked_out, *get_midi_devices_out(), command=connect_midi_out)
@@ -240,9 +290,11 @@ note_names = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "h",
               "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "h",
               "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "h"]
 
+frame_notes_area = Frame(window, padding=10)
 note_frames = []
 for i in range(11):
-    frame_notes = Frame(window)
+    frame_notes = Frame(frame_notes_area, padding=2)
+    frame_notes.pack()
     note_frames.append(frame_notes)
 
 for i in range(132):
@@ -282,10 +334,14 @@ for i in range(132):
 
     notes.append(label)
 
-# Presets
-frame_preset_area = Frame(window)
+# Settings
+frame_setting_area = Frame(window)
 
-label_presets = Label(master=frame_preset_area, text="Presets:")
+# Presets
+frame_preset_area = Frame(master=frame_setting_area, padding=3)
+frame_preset_area.pack(anchor="w")
+
+label_presets = Label(master=frame_preset_area, text="Presets:", font=("Helvetica", 15, "bold"))
 label_presets.pack(side="left")
 
 buttons_preset = []
@@ -317,7 +373,6 @@ button5 = Button(frame_presetbuttons, text=5, width=50, command=lambda: choose_p
 button5.pack(side="left")
 buttons_preset.append(button5)
 
-
 ports = range(midiin.getPortCount())
 if ports:
     for i in ports:
@@ -327,22 +382,24 @@ else:
 
 # Delay
 frame_delay = Frame(window)
-label_delay = Label(master=frame_delay, text="Delay (ms):")
+label_delay = Label(master=frame_delay, text="Delay (ms):", font=("Helvetica", 15, "bold"))
 label_delay.pack(side="left")
 entry_delay = Entry(master=frame_delay)
 entry_delay.pack(side="left")
 
 # Transpose
-frame_transpose = Frame(window)
-label_transpose = Label(master=frame_transpose, text="Transponieren:")
+frame_transpose = Frame(master=frame_setting_area, padding=3)
+frame_transpose.pack(anchor="w")
+label_transpose = Label(master=frame_transpose, text="Transponieren:", font=("Helvetica", 15, "bold"))
 label_transpose.pack(side="left")
 slider_transpose = tkinter.Scale(master=frame_transpose, from_=-3, to=3, tickinterval=1, orient=HORIZONTAL,
                                  troughcolor=color_1, background=backgroundcolor, font="Helvetica 10")
 slider_transpose.pack(side="left")
 
 # Added Octaves
-frame_added_octaves = Frame(window)
-label_added_octaves = Label(master=frame_added_octaves, text="Oktaven hinzufügen:")
+frame_added_octaves = Frame(master=frame_setting_area, padding=3)
+frame_added_octaves.pack(anchor="w")
+label_added_octaves = Label(master=frame_added_octaves, text="Oktaven hinzufügen:", font=("Helvetica", 15, "bold"))
 label_added_octaves.pack(side="left")
 checkbox_minus_2 = Checkbutton(master=frame_added_octaves, text="-2")
 checkbox_minus_2.pack(side="left")
@@ -354,20 +411,32 @@ checkbox_plus_2 = Checkbutton(master=frame_added_octaves, text="+2")
 checkbox_plus_2.pack(side="left")
 
 # Added Interval
-frame_added_interval = Frame(window)
-label_added_interval = Label(master=frame_added_interval, text="Intervall hinzufügen:")
+frame_added_interval = Frame(master=frame_setting_area, padding=3)
+frame_added_interval.pack(anchor="w")
+label_added_interval = Label(master=frame_added_interval, text="Intervall hinzufügen:", font=("Helvetica", 15, "bold"))
 label_added_interval.pack(side="left")
 clicked_intervals = StringVar()
-optionmenu_interval = OptionMenu(frame_added_interval, clicked_intervals, *get_intervals(), command=set_added_interval)
+optionmenu_interval = OptionMenu(frame_added_interval, clicked_intervals, *get_intervals_absolute(),
+                                 command=set_added_interval)
 optionmenu_interval.pack(side="left")
 clicked_up_down = StringVar()
 optionmenu_interval_up_down = OptionMenu(frame_added_interval, clicked_up_down, *get_direction(),
                                          command=set_interval_direction)
 optionmenu_interval_up_down.pack(side="left")
 
+# Tonart
+frame_tonart = Frame(master=frame_setting_area, padding=3)
+frame_tonart.pack(anchor="w")
+label_tonart = Label(master=frame_tonart, text="Tonart:", font="Helvetica 15 bold")
+label_tonart.pack(side="left")
+clicked_tonart = StringVar()
+optionmenu_tonart = OptionMenu(frame_tonart, clicked_tonart, *get_tonart(), command=set_tonart)
+optionmenu_tonart.pack(side="left")
+
 # Program Change
-frame_program_change = Frame(window)
-label_program_change = Label(master=frame_program_change, text="Prg. Change:")
+frame_program_change = Frame(master=frame_setting_area, padding=3)
+frame_program_change.pack(anchor="w")
+label_program_change = Label(master=frame_program_change, text="Prg. Change:", font=("Helvetica", 15, "bold"))
 label_program_change.pack(side="left")
 label_bank = Label(master=frame_program_change, text="Bank")
 label_bank.pack(side="left")
@@ -385,17 +454,13 @@ button_prgram_change.pack(side="left")
 
 frame_input.grid(row=0, column=0)
 frame_output.grid(row=0, column=1)
-# button_update.grid(row=0, column=3)
-frame_preset_area.grid(row=3, column=2, sticky="w")
-frame_rec_buttons.grid(row=0, column=2, sticky="w")
-# frame_delay.grid(row=14, column=0)
-frame_transpose.grid(row=4, column=2, rowspan=2, sticky="w")
-frame_added_octaves.grid(row=6, column=2, sticky="w")
-frame_added_interval.grid(row=7, column=2, sticky="w")
-frame_program_change.grid(row=8, column=2, sticky="w")
+frame_rec_buttons.grid(row=0, column=2)
+frame_setting_area.grid(row=2, column=2, rowspan=6, sticky="w")
+frame_notes_area.grid(row=2, column=0, columnspan=2)
 
-for i in range(11):
-    note_frames[i].grid(row=i + 3, column=0, columnspan=2)
+# for i in range(11):
+#     note_frames[i].grid(row=i + 3, column=0, columnspan=2)
+
 
 # Bind
 
